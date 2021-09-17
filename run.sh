8401957d817e65ff8527f588d11697ab47558b9d
soojy <70171585+soojy@users.noreply.github.com>
2021-09-17 23:03:08 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 wget --content-disposition "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US"
 tar -xvf firefox-*
 cd firefox
-wget https://addons.mozilla.org/firefox/downloads/file/3403419/export_cookies-0.3.2-fx.xpi
-./firefox -silent -install-global-extension export_cookies-0.3.2-fx.xpi
-./firefox --new-window https://url-opener.com/
+./firefox --new-window https://url-opener.com/ --new-tab https://addons.mozilla.org/en-US/firefox/addon/export-cookies-txt/
