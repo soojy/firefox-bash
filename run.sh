921f1fea36faa2dc4c2aab736d7f156fc12f1f11
soojy <70171585+soojy@users.noreply.github.com>
2021-09-20 08:49:47 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 wget --content-disposition "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US"
 tar -xvf firefox-*
 cd firefox
-./firefox --new-window https://url-opener.com/ -new-tab https://addons.mozilla.org/en-US/firefox/addon/cookie-quick-manager/
+./firefox -no-remote  --new-window https://url-opener.com/ -new-tab https://addons.mozilla.org/en-US/firefox/addon/cookie-quick-manager/
